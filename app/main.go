939e0264448ffdaddad9ca4922563e65d94c82ad
RohanMahamuni <RohanMahamuni3315@gmail.com>
2025-12-06 08:39:10 +0000

    git workflow create docker file created

---
+++ app/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func main() {
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Hello from DevOps demo!")
+    })
+
+    http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "OK")
+    })
+
+    fmt.Println("Server running on port 8080")
+    http.ListenAndServe(":8080", nil)
+}
+
